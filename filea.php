af87bd49e4a950a882748b3d8917707925cbd2f6
Diego <diegodss@gmail.com>
2017-07-12 20:15:12 -0400

    mudei em master, nao fiz commit, mudei branch e fiz commit

--- filea.php
+++ filea.php
@@ -2,3 +2,7 @@ file a
 file a has new line 
 foi necessario correcao do arquivo
 no file a tambem foi necesario alteracao 
+
+comecei a trabalhar em master, mas nao crieo o branch do meu dia de trabalho
+
+agora no filea tb fiz alteracao em dev-12-7
--- fileb.php
+++ fileb.php
@@ -1,3 +1,9 @@
 file b
 
 para arrumar o arquivo filea foi necessario alterar fileb
+
+para o branch fixfile a foi necessario outra alteracao
+
+fui no  b e ttambem fiz uma alteracao
+
+ja mudei de branch, e fiz alteracao. to na dev-12-7
